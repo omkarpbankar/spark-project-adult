bf23c4beeea46156707832c8e118c49a73f63ebd
omkarpbankar <omkarpbankar@gmail.com>
2022-09-18 00:55:55 +0530

    first commit

---
+++ logger/logger.py
@@ -0,0 +1,7 @@
+import os
+from datetime import datetime
+import uuid
+import sys
+from salary_exceptoin.salary_exceptoin import SalaryExceptoin as AppLoggerException
+from mongo_db.mongo_db_atlas import MongoDBOperation
+
