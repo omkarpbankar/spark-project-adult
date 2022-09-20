6d00beb5123cae879b685ae57a34fb06b7e6f572
omkarpbankar <omkarpbankar@gmail.com>
2022-09-21 00:06:04 +0530

    exception, mongoDB updated

---
+++ mongo_db/mongo_db_atlas.py
@@ -0,0 +1,61 @@
+import ssl
+import pymongo
+import json
+import pandas as pd
+import sys
+from adult_exception.adult_exception import AdultException as MongoDBException
+
+class MongoDBOperation:
+    def __init__(self, user_name=None,password=None):
+        try:
+            if user_name is None or password is None:
+                # Creating initial object to fetch mongoDB credentials
+                credentials = {
+                    'user_name': 'pythonbyomkar'
+                    'password': "abraka_dabra"
+                }
+                self.__user_name = credentials['user_name']
+                self.__password = credentials['password']
+            else:
+                self.__user_name = user_name
+                self.__password = password
+
+        except Exception as e:
+            mongo_db_exception = MongoDBException(
+                "Failed to instantiate mongo_db_object in module [{0}] class [{1}] method [{2}]"
+                .format(MongoDBOperation.__module__.__str__(), MongoDBOperation.__name__, "__init__"))
+            
+            raise Exception(mongo_db_exception.error_message_detail(str(e),sys)) from e
+
+    
+    def get_mongo_db_url(self):
+        """
+        return: mongo_db_url
+        """
+        try:
+            url=""
+            return url
+
+        except Exception as e:
+            mongo_db_exception=MongoDBException(
+                "Failed to fetch mongo_db url in moduel [{0}] class [{1}] method [{2}]"
+                .format(MongoDBOperation.__module__.__str__(), MongoDBOperation.__name__,
+                        self.get_mongo_db_url.__name__))
+            raise Exception(mongo_db_exception.error_message_detail(str(e),sys)) from e 
+
+    def get_databse_client_object(self):
+        """
+        Return pymongoClient object to perform action with MongoDB.
+        """
+        try:
+            url="{0}{1}".format(self.__user_name,self.__password)
+            client = pymongo.MongoClient(url,ssl_cert_reqs=ssl.CERT_NONE) # creating database client object.
+            return client
+        except Exception as e:
+            mongo_db_exception=MongoDBException(
+                "Failed to fetch data base client object in module [{0}] class [{1}] method [{2}]"
+                .format(MongoDBOperation.__module__.__str__(),MongoDBOperation.__name__,
+                self.get_databse_client_object.__name__))
+            raise Exception(mongo_db_exception.error_message_detail(str(e),sys)) from e 
+    
+    
