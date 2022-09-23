d522c82542f38df5eabf3240434f5fdb15a17569
omkarpbankar <omkarpbankar@gmail.com>
2022-09-23 05:24:29 -0700

    Utility completed

--- utility.py
+++ utility.py
@@ -51,5 +51,68 @@ def create_directory_path(path:str,is_recreated=True)-> bool:
     except Exception as e:
         raise e 
 
+def clean_data_source_dir(path, logger=None,is_logging_enable=True):
+    try:
+        if not os.path.exists(path):
+            os.mkdir(path)
+        for file in os.listdir(path):
+            if '.gitignore' in file:
+                pass
+            logger.log(f"{os.path.join(path,file)} file will be deleted.")
+            os.remove(os.path.join(path,file))
+            logger.log(f"{os.path.join(path,file)} file has been deleted")
+    except Exception as e:
+        raise E
+
+def get_logger_object_of_training(config_path:str, collection_name:str,execution_id=None, 
+                                    executed_by= None)->AppLogger:
+    config=read_params(config_path)
+    database_name=config['log_database']['training_database_name']
+    if execution_id is None:
+        execution_id=str(uuid.uuid4())
+    if executed_by is None:
+        executed_by="Omkar Bankar"
+    logger=AppLogger(project_id=2,log_databse=database_name, log_collection_name=collection_name,
+                    execution_id=execution_id, executed_by=executed_by)
+    return logger
+
+
+
+def get_logger_object_of_prediction(config_path:str, collection_name:str, execution_id=None,
+                                    executed_by=None)->AppLogger:
+    config=read_params(config_path)
+    database_name=config['log_database']['prediction_database_name']
+    if execution_id is None:
+        execution_id=str(uuid.uuid4())
+    if executed_by is None:
+        executed_by="Omkar Bankar"
+    logger=AppLogger(project_id=2,log_databse=database_name, log_collection_name=collection_name,
+                    execution_id=execution_id, executed_by=executed_by)
+    return logger
+
+def values_from_schema_function(schema_path):
+    try:
+        with open(schema_path,'r') as r:
+            dic= json.load(r)
+        
+        pattern=dic['SampleFileName']
+        length_of_date_stamp_in_file = dic["LenghthOfDateStampInFile"]
+        length_of_time_stamp_in_file = dic["LengthOfTimeStampInFile"]
+        column_name = dic["ColName"]
+        number_of_columns= dic["NumberOfColumns"]
+        
+        return pattern, length_of_date_stamp_in_file, length_of_time_stamp_in_file, column_name, number_of_columns
+    except ValueError:
+        raise ValueError
+    
+    except KeyError:
+        raise KeyError
+
+    except Exception as e:
+        raise e
+
+
+
+
 
         
